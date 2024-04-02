9ac9e2eecf9b3b1ce7212984023f467b12a13979
O-orion <lucas.lvtorres@gmail.com>
2024-04-02 18:36:19 -0300

    configurando tema

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/screens/Home';
+import { TEMAS } from './src/styles/tema';
 
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <StatusBar></StatusBar>
+    <NativeBaseProvider theme={TEMAS}>
+      <StatusBar backgroundColor={TEMAS.colors.blue[400]}></StatusBar>
       <Home></Home>
     </NativeBaseProvider>
   );
---
+++ src/styles/tema.tsx
@@ -0,0 +1,12 @@
+import  { extendTheme } from 'native-base'
+
+export const TEMAS = extendTheme ({
+    colors: {
+        blue: {
+            200: '#90e0ef',
+            400: '#00b4d8',
+            800: '#023047',
+        },
+        white: '#fff'
+    }
+})
